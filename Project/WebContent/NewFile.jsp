3ffc2594a5e3d2dedbb0e04e27af5e6af200dfa7
ravikgoyal <raviiitb619@gmail.com>
2016-03-05 17:23:10 +0530

    1st project
    my first code

---
+++ Project/WebContent/NewFile.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <title>Register form</title>
+    </head>
+    <body>
+        <form method="post" action="register">
+        Phone no:<input type="text" name="name" /><br/>
+        MMID:<input type="text" name="email" /><br/>
+        <input type="submit" value="register" />
+        </form>
+    </body>
+</html>
---
+++ Project/src/Register.java
@@ -0,0 +1,39 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet("/Register")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+	
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        out.println("You are sucessfully registered"+name+"  "+email);
+	}
+
+}
